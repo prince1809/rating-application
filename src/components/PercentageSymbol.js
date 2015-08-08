27d299d909dc400774d62eae0b9ca709ad8b9b7a
prince1809 <prince18pat@gmail.com>
2015-08-08 03:04:28 +0000

    Symbol updated

--- src/components/PercentageSymbol.js
+++ src/components/PercentageSymbol.js
@@ -3,10 +3,35 @@ var React = require('react');
 
 
 var PercentageSymbol = React.createClass({
+
   render: function(){
+    var backgroundNode = typeof this.props.background === 'string'?
+        <div className={this.props.background} /> :
+        <div style={this.props.background} />;
+
+   var iconNode = typeof this.props.icon === 'string' ?
+          <div className={this.props.icon} /> :
+          <div style={this.props.icon} />;
+
+  var iconContainerStyle = {
+    display: 'inline-block',
+    position: 'absolute',
+    overflow: 'hidden',
+    left: 0,
+    width: this.props.percent !== undefined ?
+          this.props.percent + '%' : auto
+  };
+
+  var style = {
+    display: 'inline-block',
+    position: 'relative'
+  };
     return(
-      <span>
-        *
+      <span style={style}
+        onMouseDown = {this.props.onMouseDown}
+        onMouseOver={this.props.onMouseOver}
+        >
+        {backgroundNode}
       </span>
     );
   }
